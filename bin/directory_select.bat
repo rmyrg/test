9649d240ff5a6b98d1200062e2ace4a01186896e
rmyrg <47297888+rmyrg@users.noreply.github.com>
2021-05-31 10:44:54 +0900

    Add files via upload

---
+++ bin/directory_select.bat
@@ -0,0 +1,40 @@
+@echo off
+
+setlocal ENABLEDELAYEDEXPANSION
+color 06
+set /a i=1
+
+:directory
+for /f "delims=" %%a in ('dir /b/ad') do (
+ set Arr=%%a
+ call :display
+)
+goto :input_d
+
+:display
+set Arr[%i%]=%Arr%
+set id=  %i%
+set id=%id:~-3%
+echo  %id:~-3% : !Arr[%i%]!  
+set /a i=i+1
+goto :eof
+
+:input_d
+set /p input=
+if defined input set num=%input:"=%
+set num=%num:a=1%
+set num=%num:s=2%
+set num=%num:d=3%
+set num=%num:f=4%
+set num=%num:g=5%
+set num=%num:h=6%
+set num=%num:j=7%
+set num=%num:k=8%
+set num=%num:l=9%
+set num=%num:;=0%
+set forward=!Arr[%num%]!
+endlocal && set forward=%forward%
+color 02
+if exist %forward% (
+  cd %forward%
+)
